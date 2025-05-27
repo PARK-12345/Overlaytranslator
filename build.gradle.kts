5dfd19a80206cff053e5f48a8df2e4e8a9c49123
Jin <parkjinu77@gmail.com>
2025-05-28 02:03:56 +0900

    overlaytranslator

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.10.0" apply false // 최신 AGP 버전 (2025년 5월 기준 예상)
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false // 업데이트된 Kotlin 버전
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false // 최신 Hilt 버전 (2025년 5월 기준 예상)
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false // 최신 Hilt 버전 (2025년 5월 기준 예상)
     //id("com.google.gms.google-services") version "4.4.2" apply false // Google Services (ML Kit 등)
 }
 
